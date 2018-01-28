229b769cd037b97a94a44f108e67a277e14d62bc
Santhnu Sathianathan <santhnu@gmail.com>
2018-01-28 19:49:29 +0000

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,7 +6,7 @@ class TodosController < ApplicationController
   
    def create
       #Strong Parameters introduced in Rails 4
-      #PARAMS Hash is received by our application(info from the web passes thru the URL). Inorder for us to use that Hash we need to whitelist what the appication can receive - stromg parameters 
+      #PARAMS Hash is received by our application(info from the web passes thru the URL). Inorder for us to use that Hash we need to whitelist what the appication can receive - strong parameters 
       @todo = Todo.new(todo_params)  
       
       if @todo.save
@@ -15,6 +15,7 @@ class TodosController < ApplicationController
          redirect_to todo_path(@todo.id) #or we can pass the specific attribute of the object
       else
          #render 'xxx' - will NOT clear the form/page (text that we typed in)
+         #URL for the next action('show') along with the necessary params
          render 'new'
          #redirect_to ROUTEPATH - will clear the form/page (text that we typed in)
          #redirect_to new_todo_path
@@ -25,6 +26,23 @@ class TodosController < ApplicationController
    def show
       #@todo1 = @todo.find(params[:id])
       @todo = Todo.find(params[:id])
+   end
+   
+   def edit
+      @todo = Todo.find(params[:id])
+   end
+   
+   #Automatically 'update' action is called when the form of the 'edit' action is submitted
+   def update
+      @todo = Todo.find(params[:id])
+      if @todo.update(todo_params)
+         #Message sent to the next action(Update)
+         flash[:notice] = "Todo was successfully Edited/Updated"
+         #URL for the next action('show') along with the necessary params
+         redirect_to todo_path(@todo.id)
+      else
+         render 'edit'
+      end
    end
    
    #Whitelisting Method
